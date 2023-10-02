36c8c205003cf499a1eaabcb43f48b3fe15cfc92
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2023-10-02 12:19:33 +0300

    Add files via upload

---
+++ 2 sayıyı toplama/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace _2_sayıyı_toplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSayi1 = new System.Windows.Forms.Label();
+            this.lblSayi2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSayi1
+            // 
+            this.lblSayi1.AutoSize = true;
+            this.lblSayi1.Location = new System.Drawing.Point(48, 32);
+            this.lblSayi1.Name = "lblSayi1";
+            this.lblSayi1.Size = new System.Drawing.Size(33, 13);
+            this.lblSayi1.TabIndex = 0;
+            this.lblSayi1.Text = "Sayi1";
+            this.lblSayi1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSayi2
+            // 
+            this.lblSayi2.AutoSize = true;
+            this.lblSayi2.Location = new System.Drawing.Point(48, 52);
+            this.lblSayi2.Name = "lblSayi2";
+            this.lblSayi2.Size = new System.Drawing.Size(33, 13);
+            this.lblSayi2.TabIndex = 1;
+            this.lblSayi2.Text = "Sayi2";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(47, 91);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(72, 24);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.Text = "sonuc :";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(112, 25);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(138, 20);
+            this.txtSayi1.TabIndex = 3;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(112, 52);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(138, 20);
+            this.txtSayi2.TabIndex = 4;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(51, 138);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(180, 61);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "toplamı hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 265);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lblSayi2);
+            this.Controls.Add(this.lblSayi1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSayi1;
+        private System.Windows.Forms.Label lblSayi2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
